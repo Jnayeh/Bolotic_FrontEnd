8414bff7258087273a42a3735f44ddd64fc476b1
jnayeh <mouradslim44@gmail.com>
2021-11-08 09:42:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,10 @@
 import { Component } from '@angular/core';
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Etudiant} from './etudiant';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {
-  title = 'Bolotic FrontEnd';
-  etudiants:Etudiant[] =[];
-  constructor(
-    private httpClient: HttpClient,
-    ) { }
-  async ngOnInit(){
-    await this.getAll().subscribe(res=>{
-      this.etudiants=res;
-      console.log(this.etudiants);
-    },err=>{
-      console.log(err);
-    });
-  }
-  public getAll(): Observable<any>{
-    return this.httpClient.get<Etudiant[]>('http://localhost:3000' + '/etudiants');
-  }
-  
+  title = 'Bolotic-FrontEnd';
 }
