9bf463686ce2ea06224e6b7f4071973f5afacaa6
jnayeh <mouradslim44@gmail.com>
2021-12-13 20:14:33 +0100

    contrats done

--- src/app/models/avis.ts
+++ src/app/models/avis.ts
@@ -1,11 +1,13 @@
 export class Avis {
     avis: String;
     to: String;
+    contrat: any;
     recruteur: any;
     etudiant: any;
     constructor() {
         this.avis = '';
         this.to = '';
+        this.contrat = null;
         this.recruteur = null;
         this.etudiant = null;
     }
